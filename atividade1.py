35667d4b9d2a52f2b823a4edebd030e5176d3ea7
Daniele Zancan <danny.zancan8@gmail.com>
2025-06-16 11:54:30 -0300

    commit atividade1

---
+++ atividade1.py
@@ -0,0 +1,34 @@
+# Atividade 1 - IA EDN 
+
+# 1- Programa de Saudação:
+
+print("1 - Olá, mundo!")
+
+# 2- Calculadora de Soma:
+
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print(f"2 - A soma dos números {numero1} e {numero2} é igual a: {soma}.")
+
+# 3- Calculadora de Volume
+
+comprimento = 12
+largura = 14
+altura = 20
+volume = comprimento * largura * altura
+
+print(f"3 - O volume da caixa retangular é de: {volume} cm³.")
+
+# 4- Calculadora de Preço Total:
+
+nomeProduto = "Cadeira Infantil"
+precoUn = 12.40
+quantidade = 3
+precoTotal = precoUn * quantidade
+
+print(f"4 - Produto: {nomeProduto}")
+print(f"    Preço unitário: R$ {precoUn:.2f}")
+print(f"    Quantidade: {quantidade}")
+print(f"    Preço total: R$ {precoTotal:.2f}")
