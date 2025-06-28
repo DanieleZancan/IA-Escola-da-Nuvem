49444036d45d37fff325f00e78a582345d01a30f
Daniele Zancan <danny.zancan8@gmail.com>
2025-06-28 13:06:10 -0300

    Adicionando a atividade2 - IA EDN

---
+++ atividade2.py
@@ -0,0 +1,54 @@
+# Atividade 2 - IA EDN 
+
+# 1- Conversor de Moeda:
+
+valorReais = 100.00
+taxaDolar = 5.20
+taxaEuro = 6.15
+
+print("1 - Conversor de Moeda:")
+print(f"O valor de {valorReais:.2f} reais, convertido para dólar será de: {valorReais / taxaDolar:.2f} dólares.")
+print(f"O valor de {valorReais:.2f} reais, convertido para euro será de: {valorReais / taxaEuro:.2f} euros.")
+
+print("\n")
+
+# 2 - Calculadora de Desconto:
+
+nomeProduto = "Camiseta"
+precoOriginal = 50.00
+porcentagemDesconto = 20
+
+print("2- Calculadora de Desconto:")
+print(f"O produto {nomeProduto} de R${precoOriginal:.2f} com o percentual de desconto de {porcentagemDesconto}%, terá um valor de R${precoOriginal * (porcentagemDesconto / 100):.2f} em desconto.")
+print(f"O preço final será de R${precoOriginal - precoOriginal * (porcentagemDesconto / 100):.2f}.")
+
+print("\n")
+
+# 3 - Calculadora de Média Escolar:
+
+nota1 = 7.5
+nota2 = 8.0
+nota3 = 6.5
+
+print("3- Calculadora de Média Escolar:")
+print(f"A nota 1 é: {nota1}.")
+print(f"A nota 2 é: {nota2}.")
+print(f"A nota 3 é: {nota3}.")
+print(f"A média das 3 notas é de: {(nota1 + nota2 + nota3) / 3:.2f}.")
+
+print("\n")
+
+# 4 - Calculadora de Consumo de Combustível
+
+distanciaPercorrida = 300
+combustivelGasto = 25
+
+print("4- Calculadora de Consumo de Combustível:")
+print(f"O consumo médio de um automóvel que percorreu {distanciaPercorrida} km e gastou {combustivelGasto} litros é de: {distanciaPercorrida / combustivelGasto:.2f} km por litro.")
+
+
+
+            
+
+
+
