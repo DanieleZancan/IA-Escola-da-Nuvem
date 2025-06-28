7f2b3dfb3300c3905e122033307433840163d431
Daniele Zancan <danny.zancan8@gmail.com>
2025-06-28 13:11:03 -0300

    Adicionando a atividade1 - IA EDN

--- atividade1.py
+++ atividade1.py
@@ -2,7 +2,10 @@
 
 # 1- Programa de Saudação:
 
-print("1 - Olá, mundo!")
+print("1- Programa de Saudação:")
+print("Olá, mundo!")
+
+print("\n")
 
 # 2- Calculadora de Soma:
 
@@ -10,7 +13,11 @@ numero1 = 12
 numero2 = 14
 soma = numero1 + numero2
 
-print(f"2 - A soma dos números {numero1} e {numero2} é igual a: {soma}.")
+
+print("2- Calculadora de Soma:")
+print(f"A soma dos números {numero1} e {numero2} é igual a: {soma}.")
+
+print("\n")
 
 # 3- Calculadora de Volume
 
@@ -19,7 +26,11 @@ largura = 14
 altura = 20
 volume = comprimento * largura * altura
 
-print(f"3 - O volume da caixa retangular é de: {volume} cm³.")
+
+print("3- Calculadora de Volume:")
+print(f"O volume da caixa retangular é de: {volume} cm³.")
+
+print("\n")
 
 # 4- Calculadora de Preço Total:
 
@@ -28,7 +39,9 @@ precoUn = 12.40
 quantidade = 3
 precoTotal = precoUn * quantidade
 
-print(f"4 - Produto: {nomeProduto}")
-print(f"    Preço unitário: R$ {precoUn:.2f}")
-print(f"    Quantidade: {quantidade}")
-print(f"    Preço total: R$ {precoTotal:.2f}")
+
+print("4- Calculadora de Preço Total:")
+print(f"Produto: {nomeProduto}")
+print(f"Preço unitário: R$ {precoUn:.2f}")
+print(f"Quantidade: {quantidade}")
+print(f"Preço total: R$ {precoTotal:.2f}")
